b19629289f1e5c646649486cbf5750265ac62435
LeslieZhoa <2289970030@qq.com>
2020-04-18 22:13:53 +0800

    重置第二个app布局

---
+++ 3.回答问题/智力大冲关/智力大冲关/Controler/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  智力大冲关
+//
+//  Created by Leslie Zhao on 2020/4/18.
+//  Copyright © 2020 Leslie Zhao. All rights reserved.
+//
+
+import SwiftUI
+
+struct ViewController: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ViewController_Previews: PreviewProvider {
+    static var previews: some View {
+        ViewController()
+    }
+}
---
+++ 3.回答问题/智力大冲关/智力大冲关/Model/Questions.swift
@@ -0,0 +1,17 @@
+//
+//  Questions.swift
+//  智力大冲关
+//
+//  Created by Leslie Zhao on 2020/4/18.
+//  Copyright © 2020 Leslie Zhao. All rights reserved.
+//
+
+import Foundation
+class Question{
+    let question:String
+    let answer:Bool
+    init(_ q:String, _ a:Bool) {
+        self.question = q
+        self.answer = a 
+    }
+}
